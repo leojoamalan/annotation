ec7c22085c611886a89d24e24af1208755be0472
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-17 12:57:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,23 @@ import torch
 from torchvision import transforms
 from PIL import Image
 
-# Function to load the model
-model_path = 'best_model.pth'
-model = torch.load(model_path, map_location=torch.device('cpu'))  # Load model
-model.eval()  # Set model to evaluation mode
+# # Function to load the model
+# model_path = 'best_model.pth'
+# model = torch.load(model_path, map_location=torch.device('cpu'))  # Load model
+# model.eval()  # Set model to evaluation mode
+def load_yolov5_model(weights_path):
+    # Load model
+    model = torch.hub.load('ultralytics/yolov8', 'yolov8', pretrained=False)
+    
+    # Load weights
+    checkpoint = torch.load(weights_path, map_location='cpu')['model']
+    model.load_state_dict(checkpoint.state_dict())
+    
+    # Set model in evaluation mode
+    model.eval()
+    
+    return model
+model = load_yolov5_model('best_model.pth')
 
 # Function to preprocess the image
 def preprocess_image(image):
@@ -21,7 +34,6 @@ def preprocess_image(image):
 
 # Load the model
   # Replace with your model path
-
 # Streamlit app
 st.title('Image Classification with PyTorch')
 
